2cc06aadd90b17a89f2db4974db68a2b798c3221
MinjaVizi <viziminja1509@gmail.com>
2024-01-21 09:52:41 +0100

    dodata komponenta online prodavnica

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pocetna from './Components/Pocetna';
 import Navbar from './Components/Navbar'; // Ensure the path to Navbar is correct
 import Footer from './Components/Footer'; // Ensure the path to Footer is correct
+import OnlineProdavnica from './Components/OnlieProdvnica';
 import './App.css';
 
 const App = () => {
@@ -11,6 +12,8 @@ const App = () => {
       <Navbar /> {/* Navbar component will show on every page */}
       <Routes>
         <Route path="/" element={<Pocetna />} />
+        <Route path="/online-prodavnica" element={<OnlineProdavnica />} />
+
         {/* Other routes can be added here */}
       </Routes>
       <Footer /> {/* Footer component will show on every page */}
---
+++ src/Components/OnlieProdvnica.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const OnlineProdavnica = () => {
+  return (
+    <>
+      
+
+    </>
+  );
+};
+
+export default OnlineProdavnica;
