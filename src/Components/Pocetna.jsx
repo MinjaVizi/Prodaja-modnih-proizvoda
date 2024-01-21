bd2399af5d2c54ad81e5574aae15035ec7271738
MinjaVizi <viziminja1509@gmail.com>
2024-01-21 09:37:02 +0100

    izmene

--- src/Components/Pocetna.jsx
+++ src/Components/Pocetna.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 import '../App.css';
+import pocetna from '../Images/pocetna.png'; // Ensure this path is correct
 
 const Pocetna = () => {
   return (
     <>
-      
-      <div className="content">
-        OVO JE POCETNA
+      <Navbar />
+      <div className="homepage-container">
+        <img src={pocetna} alt="pocetna" className="homepage-background" />
+        <div className="welcome-message">
+          <h1>Dobrodošli u našu modnu prodavnicu!</h1>
+          <p>Otkrijte najnovije trendove i pronađite svoj jedinstveni stil uz našu ekskluzivnu selekciju modnih proizvoda.</p>
+          <p>Kupujte sada i prepustite se svetu mode!</p>
+        </div>
       </div>
-   
+      <Footer />
     </>
   );
 };
